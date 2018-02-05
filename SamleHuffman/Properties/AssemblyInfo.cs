90a90f473247ecf760cbcdb0fd67eb97b22a5893
dmitry741 <dmitrypavlov74@gmail.com>
2018-02-05 15:29:12 +0300

    change project settings
    ridan

--- SamleHuffman/Properties/AssemblyInfo.cs
+++ SamleHuffman/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SamleHuffman")]
+[assembly: AssemblyTitle("SampleHuffman")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Danfoss")]
-[assembly: AssemblyProduct("SamleHuffman")]
-[assembly: AssemblyCopyright("Copyright © Danfoss 2018")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleHuffman")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
